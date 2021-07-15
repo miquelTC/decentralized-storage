5fb70a8a365510482803e62c0ea9d9c3fab7de54
miquelTC <miqueltc@DESKTOP-G55QVS5.localdomain>
2021-07-15 09:17:30 +0200

    create a DStorage smart contract

---
+++ src/contracts/DStorage.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract DStorage {
+  string public name = 'DStorage';
+  uint public fileCount = 0;
+  mapping(uint => File) public files;
+
+  struct File {
+    uint fileId;
+    string fileHash;
+    uint fileSize;
+    string fileType;
+    string fileName;
+    string fileDescription;
+    uint uploadTime;
+    address uploader;
+  }
+
+  event FileUploaded(
+    uint fileId,
+    string fileHash,
+    uint fileSize,
+    string fileType,
+    string fileName, 
+    string fileDescription,
+    uint uploadTime,
+    address uploader
+  );
+
+  function uploadFile(string memory _fileHash, uint _fileSize, string memory _fileType, string memory _fileName, string memory _fileDescription) public {
+    // Make sure the file exists with all its required metadata
+    require(bytes(_fileHash).length > 0);
+    require(_fileSize > 0);
+    require(bytes(_fileType).length > 0);
+    require(bytes(_fileName).length > 0);
+    require(bytes(_fileDescription).length > 0);
+    require(msg.sender!=address(0));
+
+    // Increment file id
+    fileCount ++;
+
+    // Add File to the contract
+    files[fileCount] = File(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, block.timestamp, msg.sender);
+    
+    // Trigger an event
+    emit FileUploaded(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, block.timestamp, msg.sender);
+  }
+}
+    
+
