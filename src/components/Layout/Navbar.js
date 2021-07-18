a9c05ba1db4e1f3206fc04924a827b64c8825810
miquelTC <miqueltc@DESKTOP-G55QVS5.localdomain>
2021-07-18 12:22:56 +0200

    adding alt to the image

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = (props) => {
   return (
     <nav className="navbar navbar-dark bg-primary p-0">
       <a className="navbar-brand" href="/#">
-        <img src={logo} width="40" height="40" className="align-center" alt="" />
+        <img src={logo} width="40" height="40" className="align-center" alt="logo" />
         mTC DStorage
       </a>
       <ul className="navbar-nav px-3">
